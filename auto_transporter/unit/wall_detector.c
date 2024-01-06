63808e674db25edb9477865e63bc47907b4e4a3f
2nagatomo2 <2nagatomo2@gmail.com>
2024-01-06 16:13:50 +0900

    wall_detectorのスペルミスの修正

--- auto_transporter/unit/wall_detector.c
+++ auto_transporter/unit/wall_detector.c
@@ -1,7 +1,7 @@
 #include "wall_detector.h"
 
 const int wall_detector_sensor = EV3_PORT_4;
-#define WALL_DETECTOR_THRESHOLD 100
+#define WALL_DETECTOR_THRESHOLD 200
 int wd_threshold = WALL_DETECTOR_THRESHOLD;
 
 void wall_detector_config(void) {
--- auto_transporter/unit/wall_detector.h
+++ auto_transporter/unit/wall_detector.h
@@ -2,4 +2,4 @@
 #include "util.h"
 
 extern void wall_detector_config(void);
-extern bool wall_detector_is_ditected(void);
+extern bool wall_detector_is_detected(void);
